33a2f8778d4b7c553450e2298b77f2ef687145ba
enkhjino <96076106+enkhjino@users.noreply.github.com>
2022-04-18 19:30:53 -0700

    pseudocode

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 // Remember the acronym UPS check
 
 // Step One (Understand). We have to understand the problem.
-// 1. What is the unknown?
+// 1. What is the unknown//output
+True/False
 // 2. What are the data inputs?
+two strings
 // 3. Can you restate the problem in your own words?
+is the first string in the second string?
 // 4. Can the unknown be determined from the data inputs?
+yes
 // 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory?
+sufficient
 // 6. How should I label important pieces of data input that are a part of the problem?
+str1, str2
 // 7. Draw a figure. Introduce suitable notation.
 // 8. Separate the various parts of the condition. Can you write them down?
 
 // Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found.
 // 9.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem?
+yes
 // 10.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method?
+
 // 11.    Should you introduce some auxiliary element in order to make its use possible?
 // 12. Start with a simple example. Could you restate the problem? Could you restate it still differently?
+console.log(isSubsequence('hello', 'hello world'));
+console.log(isSubsequence('sing', 'sting'));
 // 13. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs?
+
 // 14. Progress to more complex examples. What is your updated plan here to find a solution?
 // 15. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
 
 // Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
 // 16.  Can you see clearly that the step is correct?
+
+
 
 // IV. 	Step Four (Check). Examine the solution obtained and refactor.
 // 17.  Can you check your result?
@@ -31,7 +44,10 @@
 // 22.  Can you improve the performance?
 // 23.   How have other people solved this problem?
 
-function isSubsequence(array) {
-  //write code here
-  //i added this
+function isSubsequence(str1, str2) {
+  //set up a empty object
+  // split the str into an array of characters with lowercase() 
+  //iterate through the first arr1 and arr2 (for of loops)
+  //iterate with a for in loop to check if arr1[key]===arr2[key]
+
 }
